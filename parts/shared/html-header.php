df1a1f9267f8073d00297716c695c05aeaf1f365
Marvin Cespedes <marvin@mcespo.com>
2013-12-06 22:21:04 -0600

    favicon
    favicon added to the meta description

--- parts/shared/html-header.php
+++ parts/shared/html-header.php
@@ -13,7 +13,8 @@
 	<meta content="HIKE is a conference for designers looking for insight, honest answers, and solid solutions to become a professional" name="description">
 	<meta content="HIKE, Hikecon, Hike Conference, Chicago Illinois, San Francisco California" name="keywords">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
-	
+	<link rel="shortcut icon" href="favicon.ico">
+	<link rel="apple-touch-icon" href="apple-touch-icon-precomposed.png" />
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 	<script type="text/javascript" src="js/site.js"></script>
 </head>
