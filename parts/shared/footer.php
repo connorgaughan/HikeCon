ff81ae3c4962371b6427293497e05c490b1e21f8
Marvin Cespedes <marvin@mcespo.com>
2013-12-06 23:09:29 -0600

    Updated Footer Links
    All footer information and links have been updated accordingly.

--- parts/shared/footer.php
+++ parts/shared/footer.php
@@ -1,11 +1,11 @@
 <footer>
 	<div class="container">
 		<div class="logos">
-			<i class="logo icon-form-and-future"><a href="#">Test</a></i>
-			<i class="logo icon-the-secret-handshake"><a href="#">Test</a></i>
-			<i class="logo icon-adobe"><a href="#">Test</a></i>
-			<i class="logo icon-facebook"><a href="#">Test</a></i>
-			<i class="logo icon-twitter"><a href="#">Test</a></i>
+			<i class="logo icon-form-and-future"><a href="http://formandfuture.com/" title="Form and Future" alt="Form and Future website" target="_blank">Form and Future</a></i>
+			<i class="logo icon-the-secret-handshake"><a href="http://learnthesecrethandshake.com/" title="The Secret Handshake" alt="The Secret Handshake website" target="_blank">The Secret Handshake</a></i>
+			<i class="logo icon-adobe"><a href="https://www.adobe.com/" title="Adobe" alt="Adobe.com" target="_blank">Adobe</a></i>
+			<i class="logo icon-facebook"><a href="http://www.facebook.com/Hikecon" title="Hikecon Facebook Page" alt="Hikecon Facebook page" target="_blank">Hikecon Facebook</a></i>
+			<i class="logo icon-twitter"><a href="https://twitter.com/HikeCon" title="Hikecon Twitter Page" alt="Hikecon Twitter Page" target="_blank">Hikecon Twitter</a></i>
 		</div>
 	</div>
 </footer>
